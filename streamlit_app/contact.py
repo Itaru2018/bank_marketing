f209699a6e2c40b3135a6657f62666c152e01551
Itaru Yasumura <the.eggs.piero@gmail.com>
2025-07-25 15:57:13 +0200

    initial commit of Bank Marketing project

---
+++ streamlit_app/contact.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+def main():
+    st.header('Contact')
+    st.markdown('''
+                If you'd like to get in touch, feel free to reach out through any of the links below:
+                
+                ---
+
+                **Email**
+                [itaruyasumura@gmail.com](mailto:itaruyasumura@gmail.com)
+
+                **LinkedIn**
+                [https://linkedin.com/in/itaru-yasumura-27b05a1b2](https://linkedin.com/in/itaru-yasumura-27b05a1b2)
+                
+                **GitHub**
+                [https://github.com/itaru-data/bank_marketing](https://github.com/itaru-data/bank_marketing)
+
+                ''')
+
+
+# Allow this file to be tested on its own
+if __name__ == "__main__":
+    main()
